444f3e2dc7b58d8d517bda08e3c2f2b3116a7bdb
Duzy Chan <duzy@duzy.info>
2010-09-04 23:24:40 +0800

    sm.module -> sm.this

--- examples/simple_window/smart.mk
+++ examples/simple_window/smart.mk
@@ -2,24 +2,24 @@
 
 $(call sm-new-module, simple_window, executable)
 
-sm.module.compile.options := \
+sm.this.compile.options := \
   -DDS_TRACE_LEVEL=3 \
   -DDS_DEBUG_LEVEL=3 \
   -DDS_LOG_LEVEL=3 \
 
-sm.module.includes := \
+sm.this.includes := \
   -I$(ds.ui.dir)/include \
   -I$(ds.third.dir.inc) \
   -I$(ds.third.boost.dir)
 
-sm.module.libdirs := \
+sm.this.libdirs := \
   -L$(ds.ui.dir.lib) \
   -L$(ds.third.dir.lib) \
   -L$(ds.third.boost.dir.lib)
 
-sm.module.sources := simple_window.cpp
+sm.this.sources := simple_window.cpp
 
-sm.module.libs := dsui
-sm.module.rpath := $(ds.ui.dir.lib)
+sm.this.libs := dsui
+sm.this.rpath := $(ds.ui.dir.lib)
 
 $(sm-build-this)
